3f233bd3c68ed3e60cab3a7c15dd75e7f9bf4e87
Mary-14 <maria.d.artamonova@gmail.com>
2026-05-19 22:46:13 +0300

    fix warnings Codacy Static Code Analysis AuditKafkaException

--- src/main/java/company/vk/edu/distrib/compute/maryarta/kafka/AuditKafkaException.java
+++ src/main/java/company/vk/edu/distrib/compute/maryarta/kafka/AuditKafkaException.java
@@ -1,6 +1,8 @@
 package company.vk.edu.distrib.compute.maryarta.kafka;
 
 public class AuditKafkaException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public AuditKafkaException(String message, Throwable cause) {
         super(message, cause);
     }
